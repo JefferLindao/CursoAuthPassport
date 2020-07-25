725be3552bc4e2137bc9bc4adcc6ced7526f88d0
JefferLindao <jefferlindao@gmail.com>
2020-07-25 17:01:45 -0500

    Comunicacion maquina a maquina

--- ssr-server/index.js
+++ ssr-server/index.js
@@ -1,14 +1,62 @@
 const express = require('express')
 const { config } = require('./config')
-const app = express()
+const passport = require('passport')
+const boom = require('@hapi/boom')
+const cookieParse = require('cookie-parser')
+const axios = require('axios')
 
+const app = express()
 app.use(express.json())
+app.use(cookieParse())
+require('./utils/strategies/basic')
+
+const THIRTY_DAYS_IN_SEC = 2592000
+const TWO_HOURS_IN_SEC = 7200
 
 app.post("/auth/sign-in", async function (req, res, next) {
+  const { rememberMe } = req.body
+  passport.authenticate("basic", function (error, data) {
 
+    try {
+      if (error || !data) {
+        next(boom.unauthorized())
+      }
+
+      req.login(data, { session: false }, async function (error) {
+        if (error) {
+          next(error)
+        }
+
+        const { token, user } = data
+        res.cookie("token", token, {
+          httpOnly: !config.dev,
+          secure: !config.dev,
+          maxAge: rememberMe ? THIRTY_DAYS_IN_SEC : TWO_HOURS_IN_SEC
+        })
+
+        res.status(200).json(user)
+      })
+
+    } catch (error) {
+      next(error)
+    }
+  })(req, res, next);
 })
 
 app.post("/auth/sign-up", async function (req, res, next) {
+  const { body: user } = req;
+
+  try {
+    await axios({
+      url: `${config.apiUrl}/api/auth/sign-up`,
+      method: "post",
+      data: user
+    })
+
+    res.status(201).json({ message: "user created" })
+  } catch (error) {
+    next(error)
+  }
 
 })
 
